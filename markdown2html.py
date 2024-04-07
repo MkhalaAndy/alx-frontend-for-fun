dbb27d097a2734ccc8d3ced56bccb7865d0062e7
Andilemkhala <andilemkhala25@gmail.com>
2024-04-07 22:15:25 +0200

    HTML

--- markdown2html.py
+++ markdown2html.py
@@ -1,98 +1,63 @@
 #!/usr/bin/python3
 """
-Markdown to HTML Converters
+This is a script to convert a Markdown file to HTML.
+
+Usage:
+    ./markdown2html.py [input_file] [output_file]
+
+Arguments:
+    input_file: the name of the Markdown file to be converted
+    output_file: the name of the output HTML file
+
+Example:
+    ./markdown2html.py README.md README.html
 """
 
-import sys
+import argparse
+import pathlib
 import re
 
-def parse_heading(line):
-    """
-    Parse heading Markdown syntax and generate HTML
-    """
-    heading_level = line.count('#')
-    heading_text = line.strip('#').strip()
-    return f"<h{heading_level}>{heading_text}</h{heading_level}>\n"
 
-def parse_unordered_list(line):
-    """
-    Parse unordered listing Markdown syntax and generate HTML
-    """
-    items = line.strip('-').strip().split('\n')
-    html = "<ul>\n"
-    for item in items:
-        html += f"<li>{item.strip()}</li>\n"
-    html += "</ul>\n"
-    return html
+def convert_md_to_html(input_file, output_file):
+    '''
+    Converts markdown file to HTML file
+    '''
+    # Read the contents of the input file
+    with open(input_file, encoding='utf-8') as f:
+        md_content = f.readlines()
 
-def parse_ordered_list(line):
-    """
-    Parse ordered listing Markdown syntax and generate HTML
-    """
-    items = line.strip('*').strip().split('\n')
-    html = "<ol>\n"
-    for item in items:
-        html += f"<li>{item.strip()}</li>\n"
-    html += "</ol>\n"
-    return html
+    html_content = []
+    for line in md_content:
+        # Check if the line is a heading
+        match = re.match(r'(#){1,6} (.*)', line)
+        if match:
+            # Get the level of the heading
+            h_level = len(match.group(1))
+            # Get the content of the heading
+            h_content = match.group(2)
+            # Append the HTML equivalent of the heading
+            html_content.append(f'<h{h_level}>{h_content}</h{h_level}>\n')
+        else:
+            html_content.append(line)
 
-def parse_paragraph(line):
-    """
-    Parse paragraph Markdown syntax and generate HTML
-    """
-    paragraphs = line.split('\n\n')
-    html = ""
-    for paragraph in paragraphs:
-        html += f"<p>\n{paragraph.strip().replace('\n', '<br />\n')}\n</p>\n"
-    return html
+    # Write the HTML content to the output file
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.writelines(html_content)
 
-def parse_bold_and_emphasis(line):
-    """
-    Parse bold and emphasis Markdown syntax and generate HTML
-    """
-    line = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', line)
-    line = re.sub(r'__(.*?)__', r'<em>\1</em>', line)
-    return line
 
-def parse_custom_syntax(line):
-    """
-    Parse custom syntax and generate HTML
-    """
-    line = re.sub(r'\[\[(.*?)\]\]', lambda x: hashlib.md5(x.group(1).encode()).hexdigest(), line)
-    line = re.sub(r'\(\((.*?)\)\)', lambda x: x.group(1).replace('c', '').replace('C', ''), line)
-    return line
+if __name__ == '__main__':
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Convert markdown to HTML')
+    parser.add_argument('input_file', help='path to input markdown file')
+    parser.add_argument('output_file', help='path to output HTML file')
+    args = parser.parse_args()
 
-def convert_markdown_to_html(markdown_file, html_file):
-    """
-    Convert Markdown to HTML
-    """
-    with open(markdown_file, 'r') as md:
-        markdown_content = md.readlines()
-
-    with open(html_file, 'w') as html:
-        for line in markdown_content:
-            if line.startswith("#"):
-                html.write(parse_heading(line))
-            elif line.startswith("-"):
-                html.write(parse_unordered_list(line))
-            elif line.startswith("*"):
-                html.write(parse_ordered_list(line))
-            else:
-                line = parse_bold_and_emphasis(line)
-                line = parse_custom_syntax(line)
-                html.write(parse_paragraph(line))
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py <input_file.md> <output_file.html>", file=sys.stderr)
+    # Check if the input file exists
+    input_path = pathlib.Path(args.input_file)
+    if not input_path.is_file():
+        print(f'Missing {input_path}', file=sys.stderr)
         sys.exit(1)
 
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
+    # Convert the markdown file to HTML
+    convert_md_to_html(args.input_file, args.output_file)
 
-    try:
-        convert_markdown_to_html(input_file, output_file)
-    except FileNotFoundError:
-        print(f"Missing {input_file}", file=sys.stderr)
-        sys.exit(1)
-    sys.exit(0)
